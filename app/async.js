58210c7a03ad671dd4a2c34ce6ecdd273dfd6138
pptran <pptran@users.noreply.github.com>
2015-08-31 20:13:28 -0500

    Update async.js

--- app/async.js
+++ app/async.js
@@ -2,12 +2,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define([ 'jquery' ], function($) {
   return {
-    async : function(value) {
-
+      async: function (value) {
+          if (typeof value === "string") {
+              return {
+                  then: function (fn) {
+                      return "success";
+                  }
+              }
+          } else {
+              return {
+                  then: function (fn) {
+                      return true;
+                  }
+              }
+          }
     },
 
     manipulateRemoteData : function(url) {
-
+        $.get(url, function (data) {
+        });
     }
   };
 });
