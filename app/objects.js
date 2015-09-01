b0e5d4fc2f04015578aae05dabed349b78b1835e
pptran <pptran@users.noreply.github.com>
2015-08-31 20:18:15 -0500

    Update objects.js

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,29 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+        var obj = {
+               name : obj.name,
+               greeting : obj.greeting,
+               sayIt : fn
+             };
+        return obj.sayIt();
     },
 
     alterObjects : function(constructor, greeting) {
-
+        constructor.prototype.greeting = greeting;
+        return constructor;
     },
 
     iterate : function(obj) {
-
+        var result = [];
+        for (var property in obj) {
+            if (obj.hasOwnProperty(property)) {
+                var kvPair = property + ": " + obj[property];
+                result.push(kvPair);
+            }
+        }
+        return result;
     }
   };
 });
+
