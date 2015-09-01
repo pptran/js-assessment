d8a03db703394b536777c76c9e7ddfc242ffa52e
pptran <pptran@users.noreply.github.com>
2015-08-31 20:17:10 -0500

    Update modules.js

--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,18 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
+        var module = (function() {
+             pubObj = {
+                         name : str2,
+                         greeting : str1,
+                         sayIt : function() {
+                            return this.greeting + ", " + this.name;
+                        }
+             };
 
+            return pubObj;
+        }());
+        return module;
     }
   };
 });
