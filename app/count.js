6b7e04bdb396dbba346701742b179339ce0c2eec
pptran <pptran@users.noreply.github.com>
2015-08-31 20:15:27 -0500

    Update count.js

--- app/count.js
+++ app/count.js
@@ -2,7 +2,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function () {
   return {
-    count : function (start, end) {
+      count: function (start, end) {
+          var count;
+          var myVar;
+          var result = {
+              cancel: function () {
+                  clearTimeout(myVar);
+              }
+          }
+          for (count = start; count <= end ; count++) {
+               myVar = setTimeout(console.log(count), 100);
+          }
+          clearTimeout(myVar);
+          return { result };
     }
   };
 });
